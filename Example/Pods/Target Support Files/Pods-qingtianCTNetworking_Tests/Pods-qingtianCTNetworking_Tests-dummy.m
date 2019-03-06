aaefb3fa733626a896249876b8d46ccd7b1640d2
jxrs <>
2019-03-06 09:22:10 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-qingtianCTNetworking_Tests/Pods-qingtianCTNetworking_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_qingtianCTNetworking_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_qingtianCTNetworking_Tests
+@end
---
+++ Example/Pods/Target Support Files/qingtianCTNetworking/qingtianCTNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_qingtianCTNetworking : NSObject
+@end
+@implementation PodsDummy_qingtianCTNetworking
+@end
